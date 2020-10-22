d620490592e9ba933f7cc4034a3d442018cff027
treeofhope25303 <55028494+treeofhope25303@users.noreply.github.com>
2020-10-22 17:24:13 +0800

    startpointcalibrate

--- Grp8_Algo_mdp/gui/simulateRealRun.java
+++ Grp8_Algo_mdp/gui/simulateRealRun.java
@@ -39,7 +39,8 @@ public class simulateRealRun implements Runnable {
 			displayToUI();
 			mGui.displayMsgToUI("Exploration Started, Waiting for sensor data...");
 
-			sendMsg("EX|V0|(0),(0)|(0),(0)");
+			//sendMsg("EX|V0|(0),(0)|(0),(0)");
+			startExploreCalibrate();
 			
 			String recMsg = readMsg();		
 			exploreMap.setExploredCells(robot, recMsg);
